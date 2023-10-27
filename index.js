60811f7efe986a8fb766b113206e72812534f8a5
Elizabeth Kershaw <lizzykershaw99@hotmail.com>
2023-10-27 12:57:51 +1100

    first class functions

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = (drivers) => {
+    return drivers.slice(0,2);
+}
+
+const returnLastTwoDrivers = (drivers) => {
+    return drivers.slice(-2);
+}
+
+const selectingDrivers = [
+    returnFirstTwoDrivers,
+    returnLastTwoDrivers
+]
+
+const createFareMultiplier = (num1) => {
+    return fare => fare * num1
+
+}
+
+const fareDoubler = (fare) => {
+    return fare * 2
+}
+
+const fareTripler = (fare) => {
+    return fare * 3
+}
+
+const selectDifferentDrivers = (drivers, returnFirstTwoDrivers) => {
+    return returnFirstTwoDrivers(drivers)
+}
